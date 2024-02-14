8cde81acdc8900f4e45247b9d84009064349355c
Daniel Liao <danielliao0426@gmail.com>
2024-02-13 23:49:12 -0700

    update timeConfig PUT test

--- src/test/java/com/ams/restapi/timeConfig/TimeConfigControllerTest.java
+++ src/test/java/com/ams/restapi/timeConfig/TimeConfigControllerTest.java
@@ -19,58 +19,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TimeConfigControllerTest {
-        
+
     @Autowired
     private MockMvc mockMvc;
-    @Autowired private TimeConfigController controller;
+    @Autowired
+    private TimeConfigController controller;
 
-    @Test void contextLoads() throws Exception {
+    @Test
+    void contextLoads() throws Exception {
         assertNotNull(controller);
     }
 
     @Test
     void updateTimeConfig() throws Exception {
-        
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        
-
-        
         LocalTime startTime = LocalTime.of(8, 30);
         LocalTime endTime = LocalTime.of(9, 45);
         CourseInfo testCourseInfo = new CourseInfo(1234L, 1234L, "CSE 110", "COOR170",
                 List.of(DayOfWeek.MONDAY),
                 startTime, endTime);
 
-        
         LocalTime expectedBeginIn = startTime.minusMinutes(CourseInfo.DEFAULT_TOLERANCE);
         LocalTime expectedEndIn = startTime.plusMinutes(CourseInfo.DEFAULT_TOLERANCE);
         LocalTime expectedEndLate = startTime.plusMinutes(CourseInfo.DEFAULT_LATE_TOLERANCE);
         LocalTime expectedBeginOut = endTime.minusMinutes(CourseInfo.DEFAULT_TOLERANCE);
         LocalTime expectedEndOut = endTime.plusMinutes(CourseInfo.DEFAULT_TOLERANCE);
 
-        
         mockMvc.perform(put("/timeConfig/1234")
-       
                 .contentType("application/json")
                 .content(mapper.writeValueAsString(testCourseInfo)))
-                
                 .andExpect(status().isOk()).andDo(print());
 
-        
         mockMvc.perform(get("/timeConfig/1234"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
